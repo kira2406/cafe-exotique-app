e77215471ae1e8327494732ea2ee2cd3c2c2576d
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-02-21 02:38:03 +0530

    Added creating new staff functionality

--- src/components/addStaff/AddStaff.js
+++ src/components/addStaff/AddStaff.js
@@ -4,23 +4,30 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
-import './signup.css'
+import './addStaff.css'
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import KeyIcon from '@mui/icons-material/Key';
 import LoadingButton from '@mui/lab/LoadingButton'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
 import { CAFE_EXOTIQUE_API_URL } from '../constants'
 import Alert from '@mui/material/Alert'
 import EmailIcon from '@mui/icons-material/Email';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import { FormControl, InputAdornment, MenuItem, OutlinedInput } from '@mui/material'
+import { FormControl, MenuItem } from '@mui/material'
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
 
-function Signup() {
 
-    const [loginFailed, setLoginFailed] = useState(false)
+function AddStaff() {
 
-    const navigate = useNavigate()
+    const [firstname, setFirstname] = useState(null)
+    const [lastName, setLastName] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [phoneNumber, setPhoneNumber] = useState(null)
+    const [role, setRole] = useState("")
+    const [createStaffFailed, setCreateStaffFailed] = useState(false)
+    const [staffCreated, setStaffCreated] = useState(false)
+    const [loading, setLoading] = useState(false)
+
 
     const roleList = [
         {
@@ -40,6 +47,57 @@ function Signup() {
             label: 'Receptionist',
         },
     ];
+    const handleFirstnameChange = (e) => {
+        setFirstname(e.target.value)
+    }
+    const handleLastnameChange = (e) => {
+        setLastName(e.target.value)
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePhoneNumberChange = (e) => {
+        setPhoneNumber(e.target.value)
+    }
+
+    const handleRoleChange = (e) => {
+        console.log(e.target.value)
+        setRole(e.target.value)
+    }
+
+    const createStaff = () => {
+
+        if (firstname && lastName && email && phoneNumber && role) {
+            const request = {
+                "phoneNumber": phoneNumber,
+                "email": email,
+                "firstName": firstname,
+                "lastName": lastName,
+                "role": role
+            }
+
+            axios.post(`${CAFE_EXOTIQUE_API_URL}/staff/addStaff`, request)
+                .then(async res => {
+                    setCreateStaffFailed(false)
+                    console.log("Success", res.data)
+                    setStaffCreated(true)
+                    setFirstname(null)
+                    setLastName(null)
+                    setEmail(null)
+                    setPhoneNumber(null)
+                    setRole('')
+                })
+                .catch(err => {
+                    console.log(err)
+                    setCreateStaffFailed(true)
+                })
+                .finally(() => {
+                    setLoading(false)
+                })
+        }
+    }
 
     return (
         <Grid container align="center" className='content loginGrid'>
@@ -50,7 +108,11 @@ function Signup() {
                             Add new staff
                         </Typography>
                     </Grid>
-                    <Grid item xs={12} md={12} className="paddingTop40">
+
+                    <Grid item xs={12} md={12} className={staffCreated ? "paddingTop10" : "paddingTop40"}>
+                        <Grid item alignContent={'center'}>
+                            {staffCreated && <Alert severity="success">Staff created !!</Alert>}
+                        </Grid>
                         <Grid item xs={12} sm={12}>
                             <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'center' }} component="form" autoComplete='off' autoCapitalize='off'>
                                 <div >
@@ -60,6 +122,7 @@ function Signup() {
                                         variant="standard"
                                         placeholder='First Name'
                                         style={{ paddingRight: "20px" }}
+                                        onChange={handleFirstnameChange}
 
                                     />
                                     <TextField xs={10} sm={6}
@@ -67,8 +130,7 @@ function Signup() {
                                         id="lastname"
                                         variant="standard"
                                         placeholder='Last Name'
-
-
+                                        onChange={handleLastnameChange}
                                     />
                                 </div>
                             </Box>
@@ -78,6 +140,7 @@ function Signup() {
                                 component="form" autoComplete='off' autoCapitalize='off'>
                                 <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
                                 <TextField id="input-with-sx" label="Email" variant="standard" fullWidth
+                                    onChange={handleEmailChange}
                                 />
                             </Box>
                         </Grid>
@@ -86,6 +149,7 @@ function Signup() {
                                 component="form" autoComplete='off' autoCapitalize='off'>
                                 <LocalPhoneIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
                                 <TextField id="input-with-sx" label="Phone Number" variant="standard" fullWidth
+                                    onChange={handlePhoneNumberChange}
                                 />
                             </Box>
                         </Grid>
@@ -93,27 +157,31 @@ function Signup() {
                             <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'center' }}
                                 component="form" autoComplete='off' autoCapitalize='off'>
                                 <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                                <TextField
-                                    id="outlined-select-role"
-                                    select
-                                    label="Select Role"
-                                    variant='standard'
-                                    fullWidth
-                                >
-                                    {roleList.map((option) => (
-                                        <MenuItem key={option.value} value={option.value}>
-                                            {option.label}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-select-small">Select Role</InputLabel>
+                                    <Select
+                                        labelId="demo-select-small"
+                                        id="demo-select-small"
+                                        value={role}
+                                        label="Select Role"
+                                        variant='standard'
+                                        fullWidth
+                                        onChange={handleRoleChange}
+                                    >
+                                        {roleList.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                                {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
                             </Box>
                         </Grid>
                         <Grid item alignContent={'center'} className="paddingTop40 paddingBottom20">
-                            <LoadingButton variant="contained">Create</LoadingButton>
+                            <LoadingButton variant="contained" loading={loading} onClick={createStaff}>Create</LoadingButton>
                         </Grid>
                         <Grid item alignContent={'center'}>
-                            {loginFailed && <Alert severity="error">Login failed !  Check username / password</Alert>}
+                            {createStaffFailed && <Alert severity="error">Login failed !  Check username / password</Alert>}
                         </Grid>
                     </Grid>
                 </Grid>
@@ -123,4 +191,4 @@ function Signup() {
     )
 }
 
-export default Signup
+export default AddStaff
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,18 @@
 import { Typography } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
 import React from 'react'
 import ConstructionIcon from '@mui/icons-material/Construction';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+    const navigate = useNavigate();
+
+    function redirectToAddStaff() {
+        navigate('/addStaff');
+    }
+
     return (
         <Grid container align="center" className='content'>
             <Paper className='loginCard'>
@@ -14,6 +22,7 @@ function Dashboard() {
                 <Typography variant='h5'>
                     Site still under construction <ConstructionIcon />
                 </Typography>
+                <Button onClick={redirectToAddStaff} variant="contained">Add Staff</Button>
             </Paper>
         </Grid>
     )
